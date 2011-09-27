ab2af2409cf70df682e02fe88e4dc8da85c6d3bd
Txinto Vaz <txinto@gmail.com>
2011-09-27 19:35:44 +0100

    Change name from redmine_wiki_startpage to redmine_startpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,46 @@
 class WelcomeController < ApplicationController
   unloadable
  
-  before_filter :forward_to_wiki, :only => :index
+  before_filter :forward_to_startpage, :only => :index
 
-  def forward_to_wiki
-      redirect_to(:controller => "projects", :action => "wikisw", :id => "wiki")
+  def forward_to_startpage
+    
+    if (Setting.plugin_redmine_startpage['startpage_active'])
+      if (Setting.plugin_redmine_startpage['startpage_action'].blank?)
+        action_to_redirect = nil
+      else
+        action_to_redirect = Setting.plugin_redmine_startpage['startpage_action']
+      end
+    
+      if (Setting.plugin_redmine_startpage['startpage_id'].blank?)
+        id_to_redirect = nil
+      else
+        id_to_redirect = Setting.plugin_redmine_startpage['startpage_id']
+      end
+    
+      if (Setting.plugin_redmine_startpage['startpage_argname'].blank? ||
+            Setting.plugin_redmine_startpage['startpage_argvalue'].blank?)
+        argname_to_redirect = nil
+        argvalue_to_redirect = nil
+      else
+        argname_to_redirect = Setting.plugin_redmine_startpage['startpage_argname']
+        argvalue_to_redirect = Setting.plugin_redmine_startpage['startpage_argvalue']
+      end
+    
+      if (argname_to_redirect.nil?) 
+        redirect_to(
+          :controller => Setting.plugin_redmine_startpage['startpage_controller'],
+          :action => action_to_redirect,
+          :id => id_to_redirect)
+      else
+        redirect_to(
+          :controller => Setting.plugin_redmine_startpage['startpage_controller'],
+          :action => action_to_redirect,
+          :id => id_to_redirect,
+          argname_to_redirect => argvalue_to_redirect
+        )
+      end
+    end
   end
 
 end
--- init.rb
+++ init.rb
@@ -1,10 +1,19 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_wiki_startpage do
-  name 'Redmine Wiki Startpage plugin'
+Redmine::Plugin.register :redmine_startpage do
+  name 'Redmine Startpage plugin'
   author 'Txinto Vaz'
-  description 'This is a plugin for Redmine.  It allows the user to select a wiki page as start page for a Redmine website'
-  version '0.0.1'
+  description 'This is a plugin for Redmine.  It allows the user to select almost any redmine sub page as start page for a Redmine website'
+  version '0.0.2'
   url 'http://gatatac.net/projects/redminewikistartpage'
   author_url 'http://gatatac.net/users/3'
+  
+  settings :default => {
+    'startpage_active' => :false,
+    'startpage_controller'  => '',
+    'startpage_action' => '',
+    'startpage_id' => '',
+    'startpage_argname' => '',
+    'startpage_argvalue' => ''
+  }, :partial => 'settings/startpage_settings'  
 end
